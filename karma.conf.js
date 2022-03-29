007a136ce957fd2880caa5d104bf54421008b7ad
ZeroTurambar <54235564+ZeroTurambar@users.noreply.github.com>
2022-03-29 13:21:47 +0200

    Init

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/ringraziamenti_tesi_magistrale'),
+      dir: require('path').join(__dirname, './coverage/ringraziamenti'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ringraziamenti_tesi_magistrale';
+  title = 'ringraziamenti';
 }
